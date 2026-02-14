d16a3aa26964c3a22b14acc1dcb528d325e01fe2
vitorgabrieldev <vitor@clickweb.com.br>
2026-02-14 15:06:27 -0300

    feat: :white_check_mark:

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -123,7 +123,6 @@ class AuthController extends Controller
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth('api')->factory()->getTTL() * 60,
-            'user' => auth('api')->user(),
         ], $status);
     }
 }
